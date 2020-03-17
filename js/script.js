827f37bc59456b852767cfd72092dbe09787fe75
kvillamizar05 <51926023+kvillamizar05@users.noreply.github.com>
2020-03-17 11:13:36 -0500

    boton limpiar
    boton limpiar

--- js/script.js
+++ js/script.js
@@ -12,13 +12,14 @@ function leerArchivo(e) {
   }
 
   $('#Excel').prop('disabled', false);
+  $('#restart').prop('disabled', false);
 
   var lector = new FileReader();
   lector.onload = function(e) {
     var data = e.target.result;  
     var datos = data.split(/\r?\n|\r/); 
 
-    console.log(datos)
+    //console.log(datos)
     var title = datos[0].split(";");
 
     title.forEach(e => {
@@ -30,7 +31,7 @@ function leerArchivo(e) {
         var row = document.createElement('tr');
         var rowE = datos[i].split(";");
         var sectors = rowE[3].split(",");
-        console.log(sectors);
+        //console.log(sectors);
         for (let j = 0; j < rowE .length; j++) {
             var column = document.createElement("td")
             column.innerHTML = rowE[j]
@@ -66,3 +67,7 @@ function s2ab(s) {
 function botonexcel(){
   saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'Sectores.xlsx');
 };
+
+function Limpiar(){
+  location.reload();
+}
